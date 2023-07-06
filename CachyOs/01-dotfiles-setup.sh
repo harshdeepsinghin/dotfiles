65ea2d376c5aab2d93bbd873448b42818f857a0c
Harshdeep Singh <94488766+harshdeepcodes@users.noreply.github.com>
2023-07-06 18:35:35 +0530

    waybar config link and setup removed

--- CachyOs/01-dotfiles-setup.sh
+++ CachyOs/01-dotfiles-setup.sh
@@ -15,7 +15,8 @@ git clone "git@github.com:harshdeepcodes/dotfiles.git" ~/gitrepos/
 
 
 sudo rm -r /usr/share/wallpapers
-curl -o ~/Pictures/wallpapers/blue-mountains-illustration.png https://images.unsplash.com/photo-1640690821278-c6aa1e1ccee3
+wget -nc -P ~/Pictures/wallpapers/ https://harshdeepsingh.sirv.com/Wallpapers/green-clouds-open-anime-minimal.png
+wget -nc -P ~/Pictures/wallpapers/ https://harshdeepsingh.sirv.com/Wallpapers/blue-mountains-illustration.png
 
 echo """
 
@@ -27,11 +28,7 @@ echo """
 
 
 rm ~/.config/hypr/hyprland.conf
-rm ~/.config/hypr/slow.sh
-rm ~/.config/hypr/setup-scratch.sh
 ln -s ~/gitrepos/dotfiles/CachyOs/hyprland.conf ~/.config/hypr/hyprland.conf
-ln -s ~/gitrepos/dotfiles/CachyOs/slow.sh ~/.config/hypr/slow.sh
-ln -s ~/gitrepos/dotfiles/CachyOs/setup-scratch.sh ~/.config/hypr/setup-scratch.sh
 
 echo """
 
@@ -41,16 +38,16 @@ echo """
 
 """
 
-rm ~/.config/waybar/config-hypr
-ln -s ~/gitrepos/dotfiles/CachyOs/config-hypr ~/.config/waybar/config-hypr
+#rm ~/.config/waybar/config-hypr
+#ln -s ~/gitrepos/dotfiles/CachyOs/config-hypr ~/.config/waybar/config-hypr
 
-echo """
+#echo """
 
-+-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+
-!!!   WAYBAR CONFIG UPDATED   !!!
-+-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+
+#+-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+
+#!!!   WAYBAR CONFIG UPDATED   !!!
+#+-+-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+
 
-"""
+#"""
 
 sudo rm /usr/share/cachyos-fish-config/cachyos-config.fish
 sudo ln -s ~/gitrepos/dotfiles/CachyOs/cachyos-config.fish  /usr/share/cachyos-fish-config/cachyos-config.fish
