e1bf7fbcbb09ef6837fc3f0f64472205671639b6
Harshdeep Singh <94488766+harshdeepsinghin@users.noreply.github.com>
2023-08-09 12:43:12 +0000

    ssh setup added in 01-dotfiles-setup.sh

--- CachyOS/01-dotfiles-setup.sh
+++ CachyOS/01-dotfiles-setup.sh
@@ -8,6 +8,14 @@ mkdir ~/Pictures/wallpapers
 
 
 ## CLONING GITS
+chmod 700 ~/.ssh/
+chmod 600 ~/.ssh/id_rsa
+git config --global user.name "Harshdeep Singh"
+git config --global user.email "94488766+harshdeepsinghin@users.noreply.github.com"
+git config --global gpg.format ssh
+git config --global commit.gpgsign true
+git config --global user.signingkey ~/.ssh/id_rsa.pub
+ssh -T git@github.com
 git clone "git@github.com:harshdeepsinghin/dotfiles.git" ~/gitrepos/
 
 
