71b1de6743d1154f78ed84100708407d0cca0509
Harshdeep Singh <94488766+harshdeepsinghin@users.noreply.github.com>
2026-01-28 14:54:27 +0530

    fix: remove normal update because full upgrade is already happening

--- UTM/kali-linux/setup.sh
+++ UTM/kali-linux/setup.sh
@@ -7,7 +7,6 @@ sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
 echo "deb [signed-by=/etc/apt/keyrings/kali-archive-keyring.gpg] http://http.kali.org/kali kali-rolling main non-free contrib" | sudo tee /etc/apt/sources.list
 
 sudo apt update
-sudo apt upgrade -y
 sudo apt full-upgrade -y
 
 sudo apt autoremove -y
